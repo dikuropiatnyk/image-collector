1df721b4b3f1eee109cfe8f240cabb36b9dad21d
Danylo Kuropiatnyk <dikuropiatnyk@spscommerce.com>
2022-04-28 03:01:53 +0300

    Improved logging

--- main.go
+++ main.go
@@ -8,9 +8,15 @@ import (
 
 func main() {
 	start := time.Now()
+	log.Println("Start collecting...")
 	// Initialize environment
 	services.InitEnv()
+
 	encodedImages := services.CollectImagesInParallel()
+	timeOfCollecting := time.Since(start)
+	log.Printf("Image collecting time: %s\n", timeOfCollecting)
+
+	log.Println("Send images to Processor")
 	services.SendImagesToProcessor(encodedImages)
 	elapsed := time.Since(start)
 	log.Printf("Total execution time: %s\n", elapsed)
