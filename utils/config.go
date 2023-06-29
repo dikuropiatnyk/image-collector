6f53f999e4d4d0c488cd4f93f3f46c6d3734c2b6
Danylo Kuropiatnyk <dikuropiatnyk@spscommerce.com>
2023-06-29 15:47:18 +0300

    Change process endpoint URL

--- utils/config.go
+++ utils/config.go
@@ -3,6 +3,6 @@ package utils
 const RootImageURL = "https://api.unsplash.com/"
 const ImagesEndpoint = "photos?per_page="
 const ImageProcessorURL = "http://127.0.0.1:8000/"
-const ProcessEndpoint = "mongo/images/process"
+const ProcessEndpoint = "v1/images/process"
 const AccessKeyName = "ACCESS_KEY"
 const DefaultImagesCount = 30
